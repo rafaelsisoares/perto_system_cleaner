9258e2f598dfd05cd88af13ace6ee77d97693197
Rafael Soares <r.soares2015@outlook.com>
2026-03-05 20:51:45 -0300

    chore: script esta percorrendo os registros

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 from time import sleep
 import os
@@ -10,10 +12,13 @@ import os
 
 def open_browser_session() -> None:
     load_dotenv()
+    browser_options = Options()
+    browser_options.add_argument("start-maximized")
+    browser_options.add_argument("--disable-notifications")
     url = os.getenv("URL")
     email = os.getenv("EMAIL")
     password = os.getenv("PASS")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=browser_options)
     hold = WebDriverWait(browser, timeout=10)
     browser.get(url)
     hold.until(EC.visibility_of_element_located((By.ID, "inputEmail3")))
@@ -28,10 +33,12 @@ def open_browser_session() -> None:
     sub_menu.click()
     credentials_ctrl = browser.find_element(by=By.LINK_TEXT, value="Credenciado")
     credentials_ctrl.click()
-    credentials = browser.find_element(by=By.TAG_NAME, value="tbody")
+    credentials = hold.until(EC.visibility_of_element_located((By.TAG_NAME, "tbody")))
     credentials_table = credentials.find_elements(by=By.TAG_NAME, value="tr")
+    credentials_table.pop(1)
     sleep(2)
-    for credential in credentials_table:
+    for i in range(len(credentials_table)):
+        credential = credentials_table[i]
         edit_btn = credential.find_element(by=By.CLASS_NAME, value="btn-primary")
         edit_btn.click()
         nav_button = browser.find_element(by=By.LINK_TEXT, value="PORTADORES")
@@ -44,14 +51,44 @@ def open_browser_session() -> None:
         select.select_by_visible_text(options[1])
         path = "//table[@id='exportable']//tbody//tr"
         hold.until(lambda d: len(d.find_elements(By.XPATH, path)) > 1)
-        users_table = browser.find_elements(by=By.XPATH, value=path)
-        users_table.pop(0)
-        for user in users_table:
-            btn_container = user.find_elements(by=By.TAG_NAME, value="td")[-1]
-            edit = btn_container.find_element(by=By.CLASS_NAME, value="btn-primary")
-            print(edit.get_attribute("outerHTML"))
-            browser.execute_script("arguments[0].click();", edit)
-            sleep(5)
+        num_pages = browser.find_element(
+            by=By.CLASS_NAME, value="select-page"
+            ).get_attribute("max")
+        for _ in range(int(num_pages)):
+            users_table = browser.find_elements(by=By.XPATH, value=path)
+            users_table.pop(0)
+            for user in users_table:
+                btn_container = user.find_elements(by=By.TAG_NAME, value="td")[-1]
+                edit = btn_container.find_element(by=By.CLASS_NAME, value="btn-primary")
+                browser.execute_script("arguments[0].click();", edit)
+                pop_up = hold.until(EC.visibility_of_element_located((By.CLASS_NAME, "modal-content")))
+                close = pop_up.find_element(by=By.CLASS_NAME, value="btn-warning")
+                try:
+                    registers_table = hold.until(EC.visibility_of_element_located((By.TAG_NAME, "tbody")))
+                except TimeoutException:
+                    print("Este usuario não possui registros")
+                    browser.execute_script("arguments[0].click();", close)
+                    continue
+                registers = registers_table.find_elements(by=By.TAG_NAME, value="tr")
+                if len(registers) > 0:
+                    for register in registers:
+                        data = register.find_elements(by=By.TAG_NAME, value="td")
+                        print(data[0].get_attribute("textContent"))
+                        if data[0].get_attribute("textContent") == "CÓDIGO DE BARRAS":
+                            print("Este registro será excluido")
+                        print("Este registro sera preservado")
+                browser.execute_script("arguments[0].click();", close)
+                hold.until(EC.invisibility_of_element_located(pop_up))
+            if int(num_pages) > 1:
+                next_page = browser.find_element(by=By.LINK_TEXT, value=">")
+                next_page.click()
+            #sleep(3)
+        footer = browser.find_element(by=By.CLASS_NAME, value="panel-footer")
+        btn_close = footer.find_element(by=By.TAG_NAME, value="button")
+        btn_close.click()
+        sleep(3)
+        credentials = hold.until(EC.visibility_of_element_located((By.TAG_NAME, "tbody")))
+        credentials_table = credentials.find_elements(by=By.TAG_NAME, value="tr")
 
-
-open_browser_session()
+if __name__ == "__main__":
+    open_browser_session()
